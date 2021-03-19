ea07e70a3f2807e060f903925e14403f6a1856f7
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-19 17:36:22 -0400

    Add addShip skeleton, tests

--- helpers.js
+++ helpers.js
@@ -1,14 +1,30 @@
 /**
  * Returns an 2-dimensional array of null values representing a board with n rows and n columns.
  * @param  {number} n
- *         The number of rows and columns.
- * @return {Array.<Array.<null>>}
+ *         The number of rows and columns, n >= 0.
+ * @return {Array.<[null]>}
  *         A 2-dimensional n * n array of null values.
  */
 const generateBoard = (n) => {
   return new Array(n).fill(new Array(n).fill(null));
 };
 
+/**
+ * Returns a board with a given coordinate value set to 1, or false if the existing value is not null.
+ * @param  {Array.<[number|null]>} board
+ *         A 2-dimensional array representing a board.
+ * @param  {number} row
+ *         The row index.
+ * @param  {number} col
+ *         The column index.
+ * @return {Array.<[null]>|boolean}
+ *         The resulting board or false if unchanged.
+ */
+const addShip = (board, row, col) => {
+
+};
+
 module.exports = {
-  generateBoard
+  generateBoard,
+  addShip
 };
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,21 +1,57 @@
 const { assert } = require("chai");
 const {
-  generateBoard
+  generateBoard,
+  addShip
 } = require("../helpers");
 
 describe("generateBoard", () => {
   it("should return an empty erray given n = 0", () => {
-    const actual = generateBoard(0);
+    const output = generateBoard(0);
     const expected = [];
-    assert.deepEqual(actual, expected);
+    assert.deepEqual(output, expected);
   });
   it("should return an array of 3 arrays each with 3 null values given n = 3", () => {
-    const actual = generateBoard(3);
+    const output = generateBoard(3);
     const expected = [
       [null, null, null],
       [null, null, null],
       [null, null, null],
     ];
-    assert.deepEqual(actual, expected);
+    assert.deepEqual(output, expected);
+  });
+});
+
+describe("addShip", () => {
+  it("should add the ship if the current value at a coordinate is null", () => {
+    const board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    const output = addShip(board, 1, 1);
+    const expected = [
+      [null, null, null],
+      [null, 1, null],
+      [null, null, null],
+    ];
+    assert.deepEqual(output, expected);
+  });
+  it("should return false if the current value at a coordinate is 0", () => {
+    const board = [
+      [null, null, null],
+      [null, 0, null],
+      [null, null, null]
+    ];
+    const output = addShip(board, 1, 1);
+    assert.isFalse(output);
+  });
+  it("should return false if the current value at a coordinate is 1", () => {
+    const board = [
+      [null, null, null],
+      [null, 1, null],
+      [null, null, null]
+    ];
+    const output = addShip(board, 1, 1);
+    assert.isFalse(output);
   });
 });
