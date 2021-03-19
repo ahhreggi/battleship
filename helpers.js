b263e4c96dcb0efd17d9e4ad01e9a102d2e6abd6
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-19 16:56:27 -0400

    Add helpers.js, generateBoard

---
+++ helpers.js
@@ -0,0 +1,8 @@
+/**
+ * Returns an empty 10x10 board.
+ * @return {Array.<Array.<number>>}
+ *         A 2-dimensional array of zeroes, representing 10 rows and 10 columns.
+ */
+const generateBoard = () => {
+
+};
