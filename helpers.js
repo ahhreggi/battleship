a4f32a3a4647b36a0b5e0b5bf69b1e606454e87b
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-19 18:34:28 -0400

    Add attackMap skeleton, update jsdocs

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 /**
- * A player sees two boards: their own and a record of their opponent's
+ * A player sees two boards: their own and a map of their opponent's
  *
  * Player's board (starts as null with 0s for ship placements):
  *    null = empty space
@@ -22,20 +22,35 @@ const generateBoard = (n) => {
 };
 
 /**
- * Returns a new board with a given coordinate value set to 0, or false if the existing value is not null.
- * @param  {Array.<[number|null]>} board - A 2-dimensional array representing a board.
- * @param  {number} row - The row index.
- * @param  {number} col - The column index.
- * @return {Array.<[null]>|boolean} - The resulting board or false if unchanged.
+ * Given a player's board, returns a new board with a given coordinate value set to 0,
+ * or false if the existing value is not null (a ship has already been placed there).
+ * @param  {Array.<[number|null]>} playerBoard - The player's board.
+ * @param  {number} row - The row index (X coordinate).
+ * @param  {number} col - The column index (Y coordinate).
+ * @return {Array.<[number|null]>|boolean} - The player's resulting board or false if unchanged.
  */
-const addShip = (board, row, col) => {
-  if (board[row][col] !== null) {
+const addShip = (playerBoard, row, col) => {
+  if (playerBoard[row][col] !== null) {
     return false;
   }
-  const newBoard = board.map(row => row.slice()); // deep clone
+  const newBoard = playerBoard.map(row => row.slice()); // deep clone
   newBoard[row][col] = 0;
   return newBoard;
 };
+
+/**
+ * Given a player's map, returns a new board with a given coordinate set to 1 or 0 if the
+ * same coordinate on the enemy's board has a value of 0 or null, respectively (hit or miss).
+ * @param  {Array.<[number|null]>} playerMap - The player's map.
+ * @param  {Array.<[number|null]>} enemyBoard - The enemy's board.
+ * @param  {number} row - The row index (X coordinate).
+ * @param  {number} col - The column index (Y coordinate).
+ * @return {Array.<[number|null]>|boolean} - The player's resulting map or false if unchanged.
+ */
+const attackingMap = (playerMap, enemyBoard, row, col) => {
+
+};
+
 
 module.exports = {
   generateBoard,
