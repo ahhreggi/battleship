7dd83b72498fd7b8d72dac8262b669e79023e7ea
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-19 17:17:17 -0400

    Update generateBoard jsdoc

--- helpers.js
+++ helpers.js
@@ -1,9 +1,9 @@
 /**
- * Returns an empty n * n board.
+ * Returns an 2-dimensional array of null values representing a board with n rows and n columns.
  * @param  {number} n
  *         The number of rows and columns.
  * @return {Array.<Array.<null>>}
- *         A 2-dimensional array of null values representing n rows and n columns.
+ *         A 2-dimensional n * n array of null values.
  */
 const generateBoard = (n) => {
   return new Array(n).fill(new Array(n).fill(null));
