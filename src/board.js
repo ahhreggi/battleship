9f02f95fee64cae1f196ea9f10ad47bde22fcadb
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 00:43:39 -0400

    Convert Board class to Grid, create Board subclass

--- src/board.js
+++ src/board.js
@@ -9,36 +9,63 @@ const {
   getLabels,
   boardToStr
 } = require("./helpers");
-const visualKeys = require("./visualKeys");
 
-class Board {
+class Grid {
   constructor(size) {
     this.size = size;
     this.grid = generateBoard(size);
   }
 
-  // Returns a deep copy of the board.
+  // Returns a deep copy of the grid.
   deepCopy() {
     return copyBoard(this.grid);
   }
 
+  // Returns the grid with coordinate labels.
   labeled() {
     return getLabels(this.grid);
   }
 
-  toString() {
-    return boardToStr(getLabels(getBoard(this.grid)));
-  }
-
 }
 
-class Map extends Board {
+class Map extends Grid {
+
+  // Returns a visualized grid.
+  visual() {
+    return getMap(this.grid);
+  }
+
+  // Returns a string representation of the map.
   toString() {
     return boardToStr(getLabels(getMap(this.grid)));
   }
 }
 
-module.exports = { Board, Map };
+class Board extends Grid {
 
-const test = new Board(10);
-console.log(test.toString());
+  // Adds a ship at the given coordinates.
+  // coordinates = [x, y]
+  addShip(coordinates) {
+    this.grid = addShip(this.grid, coordinates);
+  }
+
+  // Returns a visualized grid.
+  visual() {
+    return getBoard(this.grid);
+  }
+
+  // Returns a string representation of the board.
+  toString() {
+    return boardToStr(getLabels(getBoard(this.grid)));
+  }
+}
+
+module.exports = { Map, Board };
+
+const p1 = new Board(10);
+p1.addShip([0, 0]); // A 0
+p1.addShip([0, 1]); // A 0
+p1.addShip([0, 2]); // A 0
+p1.addShip([0, 3]); // A 0
+p1.addShip([0, 4]); // A 0
+console.log(p1.toString());
