14b869b1f5e1aabae1c4689762162137d7c713e1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-23 01:53:49 -0400

    Update server

--- client.js
+++ client.js
@@ -14,7 +14,6 @@ const client = net.createConnection({
 client.setEncoding("utf8");
 
 client.on("data", (data) => {
-  console.log("data came back from server...");
   console.log(data);
 });
 
--- server.js
+++ server.js
@@ -4,30 +4,48 @@ const PORT = process.env.PORT || 3001;
 const net = require("net");
 const server = net.createServer();
 
-const users = []; // array of all connections
+const users = {};
+const queue = [];
 
 // Event Listeners
 server.on("connection", (client) => {
-  console.log("Someone has connected!");
   client.setEncoding("utf8");
   
-  users.push(client);
+  // Create a new client object with a unique ID
+  let id = Math.floor(Math.random() * (9999 - 1001)) + 1000;
+  while (Object.keys(users).includes(id)) {
+    id = Math.floor(Math.random() * (9999 - 1001)) + 1000;
+  }
+  
+  client.gameID = id;
+  
+  users[id] = client;
+  queue.push(id);
+  
+  const players = queue.length;
+  console.log(`Client #${client.gameID} connected. Players online: ${players}`);
+  client.write(`Welcome to Battleship! Players online: ${players}`);
+  client.write(`Enter "play" to join queue`);
 
   client.on("data", (data) => {
-    console.log(data);
-    for (let user of users) {
-      user.write(data);
-    }
+    const userInput = data.trim();
+    if (userInput === "play") {
+      client.write("Joining queue...");
+    };
   });
 
   client.on("end", () => {
-    users.splice(users.indexOf(client), 1);
-    console.log("Client disconnected.");
+    const id = client.gameID;
+    delete users[id]
+    if (queue.includes(id)) {
+      queue.splice(queue.indexOf(id), 1);
+    }
+    console.log(`Client #${id} disconnected.`);
   });
 });
 
 // Server can run only on a specific port
 // Only one server per port
 server.listen(PORT, () => {
-  console.log("Server is online...");
+  console.log(`Battleship server is online on port ${PORT}`);
 });
