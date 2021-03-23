640d4b82873e0e1e657228274b7ed8fdb4ba653c
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-23 00:56:33 -0400

    Add dotenv import

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const net = require("net");
 const stdin = process.stdin;
 stdin.setEncoding("utf8");
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const net = require("net");
 const server = net.createServer();
 const users = []; // array of all connections
